68c8526ec54b0d1e2a20a3021d5150b34df77086
Tomasz Rojek <tomekr@gmail.com>
2019-08-05 20:17:02 +0200

    Add OrderOptionIcons componet

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,15 +1,19 @@
 import {connect} from 'react-redux';
 import OrderForm from './OrderForm';
-import getOrderOptions from '../../../redux/orderRedux';
-import setOrderOptions from './../../../redux/orderRedux';
+import {getOrderOptions,setOrderOptions} from '../../../redux/orderRedux';
+//import  from './../../../redux/orderRedux';
 
 const mapStateToProps = (state) => {
-
-
   return {
     options:getOrderOptions(state),
-    setOrderOptions:setOrderOptions(state),
   };
 };
 
-export default connect(mapStateToProps)(OrderForm);
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setOrderOptions: options => setOrderOptions(options),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderForm);
--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -4,24 +4,18 @@ import {formatPrice}   from './../../../utils/formatPrice';
 import Icon from './../../common/Icon/Icon';
 
 
-const OrderOptionIcons = ({name,values, required, currentValue, setOptionValue}) => (
+const OrderOptionIcons = ({name,values,required, setOptionValue}) => (
   <div
     className={styles.icon}
-    value={currentValue}
-    onClick={event => setOptionValue(event.currentTarget.value)}
   >
     {required ? '' : (
-      <div setOptionValue='null' value='none'></div>
+      <div key='null' value=''></div>
     )}
     {values.map(value => (
-      <div className ={styles.icon}  key={value.id} value={value.id}>
-        {value.name}({formatPrice(value.price)})
-      </div>
-
+      <div  className={styles.iconActive} key={value.id} onClick={event => setOptionValue(event.currentTarget.value) } ><Icon name={value.id}/> {value.name} ({formatPrice(value.price)})</div>
     ))}
-    <Icon  name= {name}></Icon>
   </div>
-
 );
+
 
 export default OrderOptionIcons ;
